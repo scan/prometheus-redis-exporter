b7c3e4ea51a8c8cdc61b578a5a4e37b39d854b06
Tom Streller <tom.streller@functional-games.de>
2017-05-28 18:37:29 +0200

    Reduce unwraps

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
-#[macro_use] extern crate nickel;
+#[macro_use]
+extern crate nickel;
 extern crate redis;
 extern crate prometheus;
 
-use nickel::{Nickel, MediaType, HttpRouter};
-use redis::{InfoDict};
+use nickel::{Nickel, MediaType, HttpRouter, NickelError};
+use nickel::status::StatusCode;
+use redis::{InfoDict, RedisError};
 use prometheus::{Opts, Registry, Gauge, TextEncoder, Encoder};
 use std::env;
+use std::error::Error;
 
-static GAUGES: [(&'static str, &'static str, &'static str); 7] = [
-  ("connected_slaves", "connected_replicas", "Number of connected replicas"),
-  ("total_commands_processed", "total_commands_processed", "Total commands that reated this server"),
-  ("used_cpu_sys", "used_cpu_sys_seconds", "Seconds of system CPU time used"),
-  ("used_cpu_user", "used_cpu_user_seconds", "Seconds of user CPU time used"),
-  ("used_memory_peak", "used_memory_peak_bytes", "Used memory peak"),
-  ("used_memory_rss", "used_memory_rss_bytes", "Used resident memory by process"),
-  ("used_memory", "used_memory_bytes", "Used memory by process"),
-];
+static GAUGES: [(&'static str, &'static str, &'static str); 7] =
+    [("connected_slaves", "connected_replicas", "Number of connected replicas"),
+     ("total_commands_processed",
+      "total_commands_processed",
+      "Total commands that reated this server"),
+     ("used_cpu_sys", "used_cpu_sys_seconds", "Seconds of system CPU time used"),
+     ("used_cpu_user", "used_cpu_user_seconds", "Seconds of user CPU time used"),
+     ("used_memory_peak", "used_memory_peak_bytes", "Used memory peak"),
+     ("used_memory_rss", "used_memory_rss_bytes", "Used resident memory by process"),
+     ("used_memory", "used_memory_bytes", "Used memory by process")];
 
 fn get_encoded_from_registry(registry: Registry) -> String {
     let mut buffer = vec![];
     let encoder = TextEncoder::new();
     let metric_familys = registry.gather();
-    encoder.encode(&metric_familys, &mut buffer).unwrap();
 
-    String::from_utf8(buffer).unwrap()
+    return match encoder.encode(&metric_familys, &mut buffer).err() {
+               Some(e) => e.description().to_owned(),
+               _ => String::from_utf8(buffer).unwrap_or("".to_owned()),
+           };
 }
 
 fn get_redis_url() -> String {
@@ -38,27 +44,36 @@ fn get_redis_url() -> String {
     }
 }
 
-fn fetch_redis_info() -> InfoDict {
-    let client = redis::Client::open(get_redis_url().as_str()).unwrap();
-    let con = client.get_connection().unwrap();
+fn fetch_redis_info() -> Result<InfoDict, RedisError> {
+    let client = redis::Client::open(get_redis_url().as_str())?;
+    let con = client.get_connection()?;
 
-    redis::cmd("INFO").query(&con).unwrap()
+    redis::cmd("INFO").query(&con)
 }
 
 fn main() {
     let mut server = Nickel::new();
     let prometheus_prefix: String = env::var("PROMETHEUS_PREFIX").unwrap_or(String::from("redis"));
 
-    server.get("/metrics", middleware! { |_, mut res|
+    server.get("/metrics",
+               middleware! { |_, mut res|
         let registry = Registry::new();
-        let info = fetch_redis_info();
+        let info = match fetch_redis_info() {
+            Err(e) => return Err(NickelError::new(res,
+                e.description().to_owned(),
+                StatusCode::InternalServerError)), // TODO: Log
+            Ok(i) => i,
+        };
 
         for (_, &(redis_name, gauge_name, gauge_desc)) in GAUGES.iter().enumerate() {
             if !info.contains_key(&redis_name) {
                 continue;
             }
 
-            let opts = Opts::new((prometheus_prefix.clone() + "_" + gauge_name), gauge_desc.to_owned());
+            let opts = Opts::new(
+                (prometheus_prefix.clone() + "_" + gauge_name),
+                gauge_desc.to_owned()
+            );
             let gauge = Gauge::with_opts(opts).unwrap();
             let val: String = info.get(redis_name).unwrap_or(String::new());
 
